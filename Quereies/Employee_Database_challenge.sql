bd305df1038c998b6cf3a27ef98f413313ca26d5
brennan <brennan.berardo@gmail.com>
2022-10-11 08:41:58 -0400

    adding components of Deliverable 1

---
+++ Quereies/Employee_Database_challenge.sql
@@ -0,0 +1,33 @@
+-- Deliverable 1: Number of Retiring Employees by Title
+
+SELECT  e.emp_no,
+        e.first_name,
+        e.last_name,
+        t.title,
+        t.from_date,
+        t.to_date
+--INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Deliverable 1 part II: Finding unique titles
+
+SELECT DISTINCT ON (emp_no) emp_no,
+		first_name, 
+		last_name,
+		title
+--INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no;
+
+-- Deliverable 1 part III: Count of Retiring Titles
+
+SELECT COUNT(title), title
+--INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
