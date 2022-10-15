70205ad98286ff7abb43a692c7dd7bf0792f9667
brennan <brennan.berardo@gmail.com>
2022-10-15 08:21:02 -0400

    adding code for deliverable 2

--- Quereies/Employee_Database_challenge.sql
+++ Quereies/Employee_Database_challenge.sql
@@ -31,3 +31,20 @@ SELECT COUNT(title), title
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: Mentorship Eligibility
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+--INTO mentorship_elegibility
+FROM employees AS e
+LEFT OUTER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
